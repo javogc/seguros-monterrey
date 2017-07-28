8265b25ffc512cbc38e755d6ffb59b6816d2900a
Hugo Gonzalez <pinelo93@gmail.com>
2017-07-28 15:42:26 -0500

    Functional communication

--- public/js/application.js
+++ public/js/application.js
@@ -1,33 +1,50 @@
-communication = function(route_directory) {
-	this.ajax = function(command, type) {
-		route = route_directory.get_route(command.target);
+build_router = function (target) {
+	base_uri = 'http://localhost:3000/api'
+	return {
+		get_route: function (target) {
+			switch (target) {
+				case 'login':
+					return base_uri + '/login'
+					break;
+				case 'users':
+					return base_uri + '/users'
+					break;
+				default:
+					break;
+			}
+		},
+		change_base_uri: function(new_base) {
+			base_uri = new_base
+		}
+	}
+};
+
+build_communication = function(router) {
+	return {
+		ajax: function(command, type) {
+		route = command.route || router.get_route(command.target);
 		 	$.ajax({
 		 	 	url: route,
 		 	 	type: type,
 		 	 	dataType: 'json',
 		 	 	data: command.data,
-		 	 }).done(command.done || null)
-		 		 .success(command.success || null)
-		 		 .fail(command.fail || null);
+		 	 	headers: {'Authorization': command.auth_token}
+		 	 }).done(command.done)
+		 		 .success(command.success)
+		 		 .fail(command.fail);
+		},
+		get: function (command) {
+			this.ajax(command, 'GET')
+		},
+		post: function (command) {
+			this.ajax(command, 'POST')
 		}
-	this.get = function (command) {
-		this.ajax(command, 'GET')
-	};
-
-	this.post = function (command) {
-		this.ajax(command, 'POST')
-	};
+	}
 };
 
-session_handler = function(communication) {
-	this.session_token = null;
-	this.log_in = function (user, password) {
-		command = this.build_login_command(user, password)
-		communication.post(command)
-	};
-	this.log_out = function (argument) {};
-
-	this.build_login_command = function (user, password) {
+build_session_handler = function(communication) {
+	session_token = null;
+	build_login_command = function (user, password) {
 		success_fn = function(response) { this.session_token = response.auth_token }.bind(this)
 		return {
 			data: { 
@@ -35,29 +52,48 @@ session_handler = function(communication) {
 				password: password
 		 	},
 		 	success: success_fn,
-		 	target: 'login'
+		 	target: 'login',
+		 	route: 'http://localhost:3000/api/v1/login'
+		}
+	}
+
+
+	return {
+		log_in: function (user, password) {
+			command = build_login_command(user, password)
+			communication.post(command)
+		},
+		get_session_token: function() {return session_token},
+		log_out: function() {
+			throw {
+				name: 'NotImplemented',
+				message: 'This function is not yet implemented'
+			}
+		}
+
+	}
+};
+
+garciac_comms = build_garciac_comms = function (session, comms) {
+	return {
+		log_in: function(mail, password) {
+			session.log_in(mail, password)
+		},
+		post: function (model, data) {
+			command = {
+				target: model,
+				data: data,
+				auth_token: session.get_session_token(),
+				success_fn: function() {'user has been created'}
+			}
+
+			comms.post(command)
 		}
 	}
 }
 
-routes = function () {
-	this.base_uri = 'http://api.garciac.dev/api/v1'
-	this.get_route = function (target) {
-		switch (target) {
-			case 'login':
-				return this.base_uri + '/login'
-				break;
-			default:
-				break;
-		}
-	}
-}
 
-core = function(link, session) {
-	
-}
-
-garciac_routes = new routes()
-garciac_link = new communication(garciac_routes)
-session_handler = new session_handler(garciac_link)
-garciac_core = new garciac_core(garciac_link, session_handler)
+router = build_router()
+adapter = build_communication(router)
+sesh = build_session_handler(adapter)
+garciac = build_garciac_comms(sesh, adapter)
