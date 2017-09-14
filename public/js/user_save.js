922ebb0d2ce2e82901f0d14a7fac39750f25a99a
javogc <javo_gc@hotmail.com>
2017-09-14 12:04:45 -0500

    added jquery file

--- public/js/user_save.js
+++
@@ -1,7 +0,0 @@
- $( "#user-save" ).click(function() {
- 
- 	garciac.log_in('ukko', '12345', function(){ 
- 		garciac.post("users", {user: {name: $("#name").val(), mail:$("#mail").val() , password: $("#password").val() , password_confirmation: $("#password_confirmation").val() }})
- 	})
- 	
-});
