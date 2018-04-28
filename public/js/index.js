c9ed9c7a4e2be8af9084522e30d8f85f457d1de6
javogc <javo_gc@hotmail.com>
2018-04-28 15:40:40 -0500

    version casi final

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var x = document.getElementById("salud");
+    var y = document.getElementById("vida");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunctionvida() {
+    var x = document.getElementById("vida");
+    var y = document.getElementById("salud");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+    }
+}
--- saporis.rb
+++ saporis.rb
@@ -12,40 +12,21 @@ class Saporis < Sinatra::Base
 	  slim :index
 	end
 
-	get '/mision' do
-	  slim :mision
+	get '/nosotros' do
+	  slim :nosotros
 	end
 
 	get '/historia' do
 		slim :historia
 	end
 
-	get'/catsup' do
-		slim :productos_catsup
+	get'/productos' do
+		slim :productos
 	end
 
-	get'/aderezos' do
-		slim :productos_aderezos
-	end
 
-	get'/mostaza' do
-		slim :productos_mostaza
-	end
-
-	get'/quesos' do
-		slim :productos_quesos
-	end
-
-	get'/salsas' do
-		slim :productos_salsas
-	end
-
-	get'/vinagres' do
-		slim :productos_vinagres
-	end
-
-	get'/marcas' do
-		slim :marcas
+	get'/vida' do
+		slim :vida
 	end
 
 	get'/contacto' do
@@ -56,22 +37,6 @@ class Saporis < Sinatra::Base
 		  return haml :contact 
 	end
 
-	post '/contact' do
-
-		Pony.mail :to => 'javiergc93@gmail.com',
-              :from => params[:email],
-              :subject =>  params[:subject],
-              :body =>  params[:email] +" wrote:\n" + params[:message],
-              :via => :smtp,
-              :via_options => {
-                  :address              => 'smtp.gmail.com',
-                  :port                 => '587',
-                  :user_name                 => 'javiergc93@gmail.com',
-                  :password             => 'tpo3wsuf753159',
-                  :authentication       => :plain, 
-                  :domain               => "localhost.localdomain"
-                } 
-	end
 	get'/aviso_de_privacidad'do
 		slim :privacidad
 	end
